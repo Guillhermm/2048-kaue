8c81409a54fc94e303b1369d2efad0d31d007d0d
Guilherme Zeni <guillhermm_zenni@hotmail.com>
2025-08-30 14:54:05 -0300

    Modify base game to include philosophers

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,22 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+
+  if (tile.value === 2) {
+    const img = document.createElement('img');
+    img.src = "./images/marx.jpg";
+
+    inner.appendChild(img);
+  }
+  else if (tile.value === 4) {
+    const img = document.createElement('img');
+    img.src = "./images/kant.jpg";
+
+    inner.appendChild(img);
+  }
+  else {
+    inner.textContent = tile.value;
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
